4f8a861c972220236f638e7087e158749f6efffb
Alex Ordonez <ordonez.alex.pierce@gmail.com>
2015-02-08 02:14:10 -0500

    Add Facebook login

--- TeamWithMe/mobile/src/main/java/me/teamwith/teamwithme/DispatchActivity.java
+++ TeamWithMe/mobile/src/main/java/me/teamwith/teamwithme/DispatchActivity.java
@@ -1,26 +1,20 @@
 package me.teamwith.teamwithme;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
-import com.parse.Parse;
 import com.parse.ParseUser;
 
-public class DispatchActivity extends ActionBarActivity {
+public class DispatchActivity extends Activity {
+
     public static final String TAG = "TWM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         Log.d(TAG, "Reached Dispatch Activity.");
-
-        // Start Parse
-        // TODO: Extract Parse keys to api_keys.xml
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, getString(R.string.PARSE_APPLICATION_ID), getString(R.string.PARSE_CLIENT_KEY));
-        // End Parse
 
         // Start Parse Push Notifications
 //        ParsePush.subscribeInBackground("", new SaveCallback() {
